a89e456c732bd2f9e67fadc499d0fccc69dd34b2
TestingPurpos <testingpurpos01@gmail.com>
2019-03-29 11:57:57 +0530

    create base class with brouse method

---
+++ src/com/test/Amazon.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Amazon {
+
+	public static void main(String[] args) {
+		System.out.println("base class created.... ");
+
+	}
+
+	public void brouse()
+	{
+		System.out.println("brouse the git url....");
+	}
+}

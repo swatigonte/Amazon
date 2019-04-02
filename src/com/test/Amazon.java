760d19d00ec05b9b1ec3556508c3da33a1875510
TestingPurpos <testingpurpos01@gmail.com>
2019-04-02 12:36:16 +0530

    Day3 task

--- src/com/test/Amazon.java
+++ src/com/test/Amazon.java
@@ -3,12 +3,12 @@ package com.test;
 public class Amazon {
 
 	public static void main(String[] args) {
-		System.out.println("base class created.... ");
+		System.out.println("base class created...");
 
 	}
 
-	public void brouse()
+	public void basemethod()
 	{
-		System.out.println("brouse the git url....");
+		System.out.println("using brouse method....");
 	}
 }
---
+++ src/com/test/swati_day3.java
@@ -0,0 +1,15 @@
+package com.test;
+
+
+public class swati_day3 {
+	public static void main(String[] args) {
+		System.out.println("Day2 task is cloning & merging ");
+
+	}
+
+	public void basemethod()
+	{
+		System.out.println("using the cloning..");
+	}
+}
+
